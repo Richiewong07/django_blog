f79b5f6a3b07372eb48b928c70b9f6c8164cc676
Richie Wong <richiewong07@yahoo.com>
2018-03-25 14:21:39 -0500

    updated

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import path, include
-
+# from django.urls import path
+from django.conf.urls import url
+from posts import views
 
 urlpatterns = [
     url('admin/', admin.site.urls),
-    url(r'^posts/', include('posts.urls', namespace='posts')),
+    url('post/', views.post_home, name='post_home'),
 ]
--- src/posts/urls.py
+++ src/posts/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-
-    # r^$ MEANS DON'T ADD ANYTHING TO OUR URL
-    # views.index  IS WHAT YOU WANT TO DISPLAY
-    # 127.0.0.1/polls/
-    url(r'^$', views.post_home, name="post_home"),
-
-]
-
-app_name = 'posts'
+# from django.conf.urls import url
+# from . import views
+#
+# urlpatterns = [
+#
+#     # r^$ MEANS DON'T ADD ANYTHING TO OUR URL
+#     # views.index  IS WHAT YOU WANT TO DISPLAY
+#     # 127.0.0.1/polls/
+#     url(r'^$', views.post_home, name="post_home"),
+#
+# ]
+#
+# app_name = 'posts'
