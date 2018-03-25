2af52916f665c143a892c4263bb5749da54c183c
Richie Wong <richiewong07@yahoo.com>
2018-03-25 11:30:00 -0500

    updated

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
-# USING SOMETHING CALLED MVC (MODEL VIEW CONTROLLER) LOOK IT UP
+# USING SOMETHING CALLED MVC FRAMEWORK (MODEL VIEW CONTROLLER) LOOK IT UP
 
 class Post(models.Model):
     title = models.CharField(max_length = 120)
     content = models.TextField()    # TextField() IS USUALLY LONGER THAN CharField()
     updated = models.DateTimeField(auto_now = True, auto_now_add = False)        # auto_now EVERYTIME IT'S SAVED INTO THE DATABASE -> updated will be set
-    timestamp = models.DateTimeField(auto_now = False, auto_now_add = True)      # auto_now_add IS WHEN IT'S ADDED INTO DATABASE, ONLY SET 1 TIME
+    timestamp = models.DateTimeField(auto_now = False, auto_now_add = True)      # auto_now_add IS WHEN IT'S ADDED INTO DATABASE INITIALLY, ONLY SET 1 TIME (SAVE AND SET)
 
     def __str__(self):              # UNICODE TO RETURN
         return self.title
