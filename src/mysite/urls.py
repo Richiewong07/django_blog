e6d4cb6a593c9c00a4bf36969d7e14c6c1800211
Richie Wong <richiewong07@yahoo.com>
2018-03-25 14:45:31 -0500

    updated

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-# from django.urls import path
 from django.conf.urls import url
-from posts import views
+
+from posts import views as post_views
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
-    url('post/', views.post_home, name='post_home'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^post/$', post_views.post_home, name = 'post'),  # post/$ ANYTHING BEYOND IT DOES NOT DEAL WITH THIS PATTERN
 ]
