e52eedcd844549f5a0002e1000292f49edd49187
Richie Wong <richiewong07@yahoo.com>
2018-03-28 19:26:08 -0500

    updated

--- src/posts/views.py
+++ src/posts/views.py
@@ -2,12 +2,29 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 
 
+
 # Create your views here.
 
+from .forms import PostForm
 from .models import Post
 
 def post_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit = False)
+        instance.save()
+
+
+    # if request.method == "POST":
+    #     print(request.POST.get("content"))
+    #     print(request.POST.get("title"))
+    #
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "post_form.html", context)
 
 
 
