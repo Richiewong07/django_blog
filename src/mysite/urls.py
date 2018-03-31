766ae4fb72e43493123662d894300b6cf07706f9
Richie Wong <richiewong07@yahoo.com>
2018-03-30 22:34:42 -0500

    updated

--- src/mysite/urls.py
+++ src/mysite/urls.py
@@ -18,9 +18,17 @@ from django.conf.urls import include, url
 
 # from posts import views as post_views
 
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib import admin
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^posts/', include("posts.urls", namespace='posts')),  # include ALLOWS US TO IMPORT URL
 
     # url(r'^post/$', include(post_views.post_home, name = 'post')),  # post/$ ANYTHING BEYOND IT DOES NOT DEAL WITH THIS PATTERN
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
