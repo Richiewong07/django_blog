dce3f411786f24cc2115a9c0ce0c86fab88cd795
Richie Wong <richiewong07@yahoo.com>
2018-04-02 19:26:16 -0500

    updated

--- src/posts/models.py
+++ src/posts/models.py
@@ -4,11 +4,22 @@ from django.urls import reverse
 # Create your models here.
 # USING SOMETHING CALLED MVC FRAMEWORK (MODEL VIEW CONTROLLER) LOOK IT UP
 
+
+# CHANGES LOCATION OF UPLOAD, ADD IT TO MODEL
+def upload_location(instance, filename):
+    return "%s/%s" %(instance.id, filename)
+
 class Post(models.Model):
     title = models.CharField(max_length = 120)
-    image = models.FileField(null = True, blank = True)
+    image = models.ImageField(upload_to = upload_location,
+                              null = True,
+                              blank = True,
+                              width_field = "width_field",
+                              height_field = "height_field")
+    height_field = models.IntegerField(default = 0)
+    width_field = models.IntegerField(default = 0)
     content = models.TextField()    # TextField() IS USUALLY LONGER THAN CharField()
-    updated = models.DateTimeField(auto_now = True, auto_now_add = False)        # auto_now EVERYTIME IT'S SAVED INTO THE DATABASE -> updated will be set
+    updated = models.DateTimeField(auto_now = True,auto_now_add = False)        # auto_now EVERYTIME IT'S SAVED INTO THE DATABASE -> updated will be set
     timestamp = models.DateTimeField(auto_now = False, auto_now_add = True)      # auto_now_add IS WHEN IT'S ADDED INTO DATABASE INITIALLY, ONLY SET 1 TIME (SAVE AND SET)
 
     def __str__(self):              # UNICODE TO RETURN TITLE IN ADMIN PAGE
