91b8ecf9d0cc4ea6b00990f7f8a49f21d208c4bb
Richie Wong <richiewong07@yahoo.com>
2018-04-07 19:01:18 -0500

    updated

--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mc9k6%s46v=3+*u_lyx+5glq*83ys%sk#v3yl1ma_p$@tbu!rp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
